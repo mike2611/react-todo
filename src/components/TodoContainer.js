a6a6f5f689216adbb22a72bffed34234eaf36a89
mike2611 <mpuentes2611@gmail.com>
2021-10-26 14:05:09 -0500

    Add .babelrc

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import TodoList from './TodoList.js';
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import ReactDOM from 'react-dom';
 import TodoContainer from './components/TodoContainer.js';
 
