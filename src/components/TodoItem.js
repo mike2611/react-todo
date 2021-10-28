f47326539cc86eefc34bef334f12088ecc9bd966
mike2611 <mpuentes2611@gmail.com>
2021-10-27 23:05:01 -0500

    Add module.css for TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import styles from './TodoItem.module.css';
 
 class TodoItem extends React.PureComponent {
   render() {
     const { todo, handleChangeProps, deleteTodoProps } = this.props;
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type="checkbox"
+          className={styles.checkbox}
           checked={todo.completed}
           onChange={() => handleChangeProps(todo.id)}
         />
