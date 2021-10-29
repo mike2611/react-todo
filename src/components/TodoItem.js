0d2c583bcbf83af7d99f822b263029cdce5e1402
mike2611 <mpuentes2611@gmail.com>
2021-10-28 20:47:58 -0500

    Add edit to input in TodoItems.js dile

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,20 @@
 import React from 'react';
 import styles from './TodoItem.module.css';
 
-class TodoItem extends React.PureComponent {
+class TodoItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+    };
+  }
+
+  handleEditing = () => {
+    this.setState((prevState) => ({
+      editing: !prevState.editing,
+    }));
+  }
+
   render() {
     const completedStyle = {
       fontStyle: 'italic',
@@ -12,18 +25,37 @@ class TodoItem extends React.PureComponent {
     };
     const { todo, handleChangeProps, deleteTodoProps } = this.props;
     const { completed, id, title } = todo;
+    const { editing } = this.state;
+    const viewMode = {};
+    const editMode = {};
+    if (editing) {
+      viewMode.display = 'none';
+    } else {
+      editMode.display = 'none';
+    }
     return (
       <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => handleChangeProps(id)}
+          />
+          <button type="button" onClick={() => deleteTodoProps(id)}>Delete</button>
+          <span style={completed ? completedStyle : null}>
+            {title}
+          </span>
+        </div>
         <input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => handleChangeProps(id)}
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={title}
+          onChange={(e) => {
+            console.log(e.target.value, id);
+          }}
         />
-        <button type="button" onClick={() => deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>
-          {title}
-        </span>
       </li>
     );
   }
